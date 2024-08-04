04d95001e4a67f3a0f442a87e5b449214f286b04
xiuhui_coder <jxs5477@163.com>
2024-08-04 10:44:00 +0800

    基础

---
+++ Source/rts/rts.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "rts.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, rts, "rts" );
+
+DEFINE_LOG_CATEGORY(Logrts)
+ 
---
+++ Source/rts/rtsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "rtsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ArtsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ArtsGameMode();
+};
+
+
+
